71270ef88ad5ef660dd96c311bbd6ad8b0593646
Kathy Yang <kayang@princeton.edu>
2022-06-08 23:11:12 -0400

    end of night 1

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
+import "./About.css";
+import my_selfie from "../assets/my_selfie.jpg";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div class="split left">
+          <div className="centered">
+            <img 
+              className="profile_image"
+              // Image goes here
+              src={my_selfie}
+              alt="profile pic"
+              ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Kathy Yang</div>
+            <div className="brief_description">
+              Hello! I'm a rising junior at Princeton studying Computer Science,
+              Technology & Society, and American Studies. I'm passionate
+              about leveraging technical and sociological knowledge to
+              build harmonious, ethical, and accessible systems that facilitate
+              public wellbeing. This summer, in addition to participating
+              in the Software Development track of IBM Accelerate, I am 
+              interning within the Solution Design Group at DataArt, an 
+              IT company that specializes in building custom software for 
+              clients. 
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
 }
+
